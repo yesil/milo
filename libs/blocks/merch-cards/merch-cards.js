0c3afb8c875db096ad9e52e90670fb9743a4baef
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2024-03-02 00:27:52 +0100

    perf opt

--- libs/blocks/merch-cards/merch-cards.js
+++ libs/blocks/merch-cards/merch-cards.js
@@ -255,6 +255,5 @@ export default async function init(el) {
   await merchCards.updateComplete;
   performance.mark('merch-cards-render:end');
   performance.measure('merch-cards-render', 'merch-cards-render:start', 'merch-cards-render:end');
-  await makePause(100);
   return merchCards;
 }
