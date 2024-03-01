5bb80b6af2144753c4cd7d85e36332d011d717ca
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2024-03-02 00:06:52 +0100

    limit cards

--- libs/blocks/merch-cards/merch-cards.js
+++ libs/blocks/merch-cards/merch-cards.js
@@ -63,7 +63,7 @@ async function fetchCardsData(config, type, el) {
   let cardsData;
   let err;
   try {
-    const res = await fetch(`${config?.locale?.prefix ?? ''}${config.queryIndexCardPath}.json?sheet=${type}`);
+    const res = await fetch(`${config?.locale?.prefix ?? ''}${config.queryIndexCardPath}.json?sheet=${type}&limit=6`);
     if (res.ok) {
       cardsData = await res.json();
     } else {
